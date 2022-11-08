5d5477b13482405d29f74b7417f590601b6f985b
Andres Silva <felipe1998157@gmail.com>
2022-11-08 09:23:04 -0500

    first commit

---
+++ src/main/java/org/example/programming/Functional.java
@@ -0,0 +1,36 @@
+package org.example.programming;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Functional {
+    public void printAllNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(Functional::print);
+    }
+
+    public void printAllNumbersInListFunctionalWithLambda(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(number -> System.out.println(number));
+    }
+
+    public void printEventNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(Functional::isEvent)
+                .forEach(Functional::print);
+    }
+
+    public void printEventNumbersInListFunctionalWithLambdas(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(number -> System.out.println(number));
+    }
+
+    public static void print(int number) {
+        System.out.println(number);
+    }
+
+    public static Boolean isEvent(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ src/main/java/org/example/programming/Structured.java
@@ -0,0 +1,20 @@
+package org.example.programming;
+
+import java.util.List;
+
+public class Structured {
+
+    public void printAllNumbersInListStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public void printEventNumbersInStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
